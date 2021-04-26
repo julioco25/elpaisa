69ab09d8926c7175d079179c06dab3f7ba5510bf
Javier-Ayala <l17420517@jiquilpan.tecnm.mx>
2021-04-26 12:52:35 -0500

    Create Slider.jsx

---
+++ elpaisa/src/components/Slider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Slider= () =>{
+    return(
+        <div className="presentador">
+            <img src="./img/slider-dogs.png" alt="#"/>
+        </div>
+    );
+}
+
+export default Slider
