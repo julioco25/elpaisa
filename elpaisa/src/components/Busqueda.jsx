d5df2205adcc2be2f92d815e806ff5410993372d
Javier-Ayala <l17420517@jiquilpan.tecnm.mx>
2021-04-26 12:45:43 -0500

    Update Busqueda.jsx

---
+++ elpaisa/src/components/Busqueda.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Busqueda= () =>{
+    return(
+        <div className="mini">
+            <input type="search" className="buscar" placeholder="Buscar..." />
+            <a href="#"><img src="./img/lupa.png" alt=""/></a>
+        </div>
+    );
+}
+
+export default Busqueda
