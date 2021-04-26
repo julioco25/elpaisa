f0823d90bce7f8e2e6e8abf6aca84604afaec433
jacoco25 <l17420528@jiquilpan.tecnm.mx>
2021-04-26 12:36:58 -0500

    entrar

--- elpaisa/src/App.jsx
+++ elpaisa/src/App.jsx
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import './Style/upnfm.css'
+import Header from './Componentes/Header'
+import Presentador from './Componentes/Presentador'
+import Busqueda from './Componentes/Busqueda';
+import Informacion from './Componentes/Informacion';
+import Footer from './Componentes/Footer'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header/>
+    <Presentador/>
+    <Busqueda/>
+    <Informacion/>
+    <Footer/>
     </div>
   );
 }
