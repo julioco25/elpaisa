3a8adba507c12fad4f2f276a9dd180fd82fc04c1
jacoco25 <l17420528@jiquilpan.tecnm.mx>
2021-04-26 23:36:28 -0500

    entrar

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -4,56 +4,70 @@ import '../Style/productos.css'
 const Productos= () =>{
     return(
         <section className="productos">
+            <br/>
+            <p>Categorias Principales</p>
     <div className="Primero">
         <div className="unouno">
             <article>
                 <figure>
-                    <img src="./img/m4.png" />
-                    <figcaption><strong>CACHORRO</strong></figcaption>
+                    <img src="./img/perroncachorro.jpg" />
+                    <figcaption><strong>Cachorro</strong></figcaption>
                 </figure>
             </article>
         </div>
         <div className="unodos">
             <article>
                 <figure>
-                    <a href="JJG~SW1.html"><img src="./img/m4.png" /></a>
-                    <figcaption><strong>ADULTO</strong></figcaption>
+                    <a href="JJG~SW1.html"><img src="./img/gatina.jpg" /></a>
+                    <figcaption><strong>Gatos</strong></figcaption>
                 </figure>
             </article>
         </div>
         <div className="unotres">
             <article>
                 <figure>
-                    <img src="./img/m4.png" />
-                    <figcaption><strong>SENIOR</strong></figcaption>
+                    <img src="./img/canpro25.jpg" />
+                    <figcaption><strong>Adultos</strong></figcaption>
                 </figure>
             </article>
         </div>
     </div>
 {/* --------------------------------------------------- */}
-    <div className="Segunda">
+     <p>Productos Destacados</p>
+     <br/>
+     <div className="Segunda">
+        <br/>
+        
+       
             <div className="dosuno">
                 <figure>
-                    <img src="./img/m4.png" />
-                    <figcaption><strong>CACHORRO RAZA PEQUEÑA</strong></figcaption>
+                    <img src="./img/perronadulto.jpg" />
+                    <figcaption><strong>PerrOn Adulto</strong><br/>
+                    <del>$201</del><e>189</e>
+                    </figcaption>
                 </figure>
             </div>
 
             <div className="dosdos">
                 <figure>
-                    <img src="./img/m4.png" />
-                    <figcaption><strong>ADULTO RAZA PEQUEÑA</strong></figcaption>
+                    <img src="./img/fiduspro.jpg" />
+                    <figcaption><strong>Fidus Pro Adultos</strong><br/>
+                    <del>$201</del><e>189</e>
+                    </figcaption>
                 </figure>
             </div>
 
             <div className="dostres">
                 <figure>
-                    <a href="JJG~SW2.html"><img src="./img/m4.png" /></a>
-                    <figcaption><strong>SENIOR RAZA PEQUEÑA</strong></figcaption>
+                    <a href="JJG~SW2.html"><img src="./img/campidog.webp" /></a>
+                    <figcaption><strong>Campidog Adultos</strong><br/>
+                    <del>$201</del><e>189</e>
+                    </figcaption>
                 </figure>
+                
             </div>
         </div>
-
+<br/><br/>
     </section>
     );
 }
